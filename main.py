1a952b0a01bcab4c883cb12c7826501f022d6c8a
Marcin Łesek <daav95@gmail.com>
2019-01-23 02:00:39 +0100

    Add layers to main

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import pandas
 from tqdm import tqdm
 from gensim.models import KeyedVectors
+from keras.models import Sequential
+from keras.layers import Bidirectional, LSTM, Dense
+
 from modules.createBatchGenerator import createBatchGenerator
 
 tqdm.pandas()
 
 googleNewsPath = './data/embeddings/GoogleNews-vectors-negative300/GoogleNews-vectors-negative300.bin'
 trainSetPath = './data/train.csv'
-batchSize = 128
+trainBatchSize = 128
+
+layerUnits = 64
+trainEpochs = 5
+trainStepsPerEpoch = 250
 
 trainSet = pandas.read_csv(trainSetPath)
-embeddings = KeyedVectors.load_word2vec_format(googleNewsPath, binary=True)
+embeddings = tqdm(KeyedVectors.load_word2vec_format(googleNewsPath, binary=True))
 
-batchGenerator = createBatchGenerator(trainSet, embeddings, batchSize)
-print(next(batchGenerator)) # Print first batch
+trainBatchGenerator = createBatchGenerator(trainSet, embeddings, trainBatchSize)
+
+
+model = Sequential()
+model.add(Bidirectional(LSTM(layerUnits, return_sequences=True),
+                        input_shape=(30, 300)))
+model.add(Bidirectional(LSTM(layerUnits)))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit_generator(trainBatchGenerator(trainSet), epochs=trainEpochs,
+                    steps_per_epoch=trainStepsPerEpoch,
+                    validation_data=(),
+                    verbose=True)
