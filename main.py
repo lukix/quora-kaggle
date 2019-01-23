460e8cb283b33558aa2f4aa65a6c112c6228ce00
lukaszjenczmyk <lukasz.jenczmyk@brainhub.pl>
2019-01-23 01:24:08 +0100

    Add batch generator

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import math
+import pandas
+import numpy as np
+from tqdm import tqdm
+from gensim.models import KeyedVectors
 from modules.wordsToVectors import wordsToVectors
-from modules.truncateQuestionsData import truncateQuestionsData
+
+tqdm.pandas()
 
 googleNewsPath = './data/embeddings/GoogleNews-vectors-negative300/GoogleNews-vectors-negative300.bin'
 trainSetPath = './data/train.csv'
-shouldPrintCoverageData = False
 
-questionsVectorData = wordsToVectors(googleNewsPath, trainSetPath, shouldPrintCoverageData)
+trainSet = pandas.read_csv(trainSetPath)
+embeddings = KeyedVectors.load_word2vec_format(googleNewsPath, binary=True)
 
-truncatedQuestionsData = truncateQuestionsData(questionsVectorData)
+def createBatchGenerator(trainData):
+    batchSize = 128
+    numberOfBatches = math.ceil(len(trainData) / batchSize)
+    while True: 
+        trainData = trainData.sample(frac=1.)  # Shuffle the data.
+        for i in range(numberOfBatches):
+            questions = trainData.iloc[i * batchSize : (i + 1) * batchSize, 1]
+            questionsArray = np.array(wordsToVectors(embeddings, questions))
+            yield questionsArray, np.array(trainData["target"][i * batchSize:(i + 1) * batchSize])
+
+batchGenerator = createBatchGenerator(trainSet)
+print(next(batchGenerator)) # Print first batch
